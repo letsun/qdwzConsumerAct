12e09f2e221b2501859774c01f6427d896de0160
ENOKEEIRYIBV6MZ\Administrator <653353427@qq.com>
2019-09-16 15:37:59 +0800

    111

--- public/quguanggao/scripts/index_act.js
+++ public/quguanggao/scripts/index_act.js
@@ -3,7 +3,8 @@ var ip2 = 'https://advp-api.lxcyhd.com/adv';
 var adVApi = {
     advertisement: ip2 + '/platAdv/getAdvByAdvPositionName1', 					// 获取广告图
     pageRecord: ip2 + '/platAdv/addPageRecord', 									// 广告页面浏览统计
-    browseRecord: ip2 + '/platBrowseRecord/browseRecord' 						// 广告点击浏览统计
+    browseRecord: ip2 + '/platBrowseRecord/browseRecord', 						// 广告点击浏览统计
+    browseClickRecord: ip2 + '/platBrowseRecord/browseClickRecord' 						// 广告统计
 };
 $(function () {
     // 开始活动
@@ -13,19 +14,15 @@ $(function () {
             // 活动页面广告
             getAdvByAdvPositionName(productName,function (red) {
                 var href = red.data[0].linkUrl;
-                browseRecord(0,red.data[0].advId,function () {
-                    browseRecord(1,red.data[0].advId,function () {
-                        window.location.href = href;
-                    });
+                browseClickRecord(red.data[0].advId,function () {
+                    window.location.href = href;
                 });
             });
         } else {
             getAdvByAdvPositionName('二次进入广告位',function (red) {
                 var href = red.data[0].linkUrl;
-                browseRecord(0,red.data[0].advId,function () {
-                    browseRecord(1,red.data[0].advId,function () {
-                        window.location.href = href;
-                    });
+                browseClickRecord(red.data[0].advId,function () {
+                    window.location.href = href;
                 });
             });
         }
@@ -56,6 +53,24 @@ function browseRecord(type,advId,callback) {
         type: 'GET',
         data: {
             operationType: type,
+            city: '',
+            district: '',
+            province: '',
+            advId: advId,
+        },
+        headers: getHeader(),
+        dataType: 'json',
+        success: function(res) {
+            callback();
+        }
+    });
+}
+
+function browseClickRecord(advId,callback) {
+    $.ajax({
+        url: adVApi.browseClickRecord,
+        type: 'GET',
+        data: {
             city: '',
             district: '',
             province: '',
