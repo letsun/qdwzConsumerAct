1a544875aaa6a841cbf0f1436357c0a4722f1729
huyao95 <huyao95@example.com>
2019-10-30 18:23:09 +0800

    东方红修改

--- public/dongfanghongNew/scripts/Func.js
+++ public/dongfanghongNew/scripts/Func.js
@@ -3,7 +3,7 @@ var Func = {};
 $(function() {
 
 	/**
-	 * 查找二维码情况
+	 * 防伪溯源展示接口
 	 * @param  function callback 查找后的回调
 	 * @return null
 	 */
@@ -25,63 +25,54 @@ $(function() {
 	var isLottery = false;
 
 	/**
-	 * 去抽奖
-	 * @param { } [varname] [description]
+	 * 抽奖
+	 * @param  function callback 抽奖后的回调函数
 	 * @return null
 	 */
-	Func.toLottery = function(url,callback) {
+	Func.lottery = function(url, callback) {
+		$('#loadingWrapper').show();
+		if (isLottery) {
+			return;
+		}
+
+		isLottery = true;
+
 		$.ajax({
 			url: url,
 			type: 'GET',
 			headers: getHeader(),
 			dataType: 'json',
 			success: function(res) {
-                $('#loadingWrapper').hide();
-				var html = '';
-				var data = res.data;
-
-				$('#actLoading').fadeOut();
-
-				if (res.code === 200) {
-
-					if (res.data.prizeType === 0) {
-						Global.requestTempByAjax('/nantian/temp/dan.ejs', res.data, function(template) {
-
-							$('#resultContent').html(template);
-							callback(res);
-						});
-					}
-
-				} else if (res.code === 201) {
-
-					Func.fail(res, callback);
-
-				} else {
-					common.alert({
-						content: res.msg,
-						mask: true
-					});
-				}
+				isLottery = false;
+				callback(res);
 			}
 		});
 	}
 
-	Func.fail = function(res, callback) {
-		
-		$('#resultContainer').css({
-			'background': 'url("https://qdwzvue-1254182596.cos.ap-guangzhou.myqcloud.com/qdwzAct/commonAct_2/images/5/1_17.png") no-repeat'
-		});
-		Global.requestTempByAjax('/nantian/temp/error.ejs', res, function(template) {
+	/**
+	 * 查找二维码情况
+	 * @param  function callback 查找后的回调
+	 * @return null
+	 */
+	Func.findActivityByEncode = function(callback) {
+		$('#loadingWrapper').show();
+		$.ajax({
+			url: api.findActivityByEncode,
+			type: 'GET',
+			headers: getHeader(),
+			dataType: 'json',
+			success: function(res) {
+				$('#loadingWrapper').hide();
+				callback(res);
+			},
+			error:function () {
 
-			$('#resultContent').html(template);
-
-			$('#getAwardBtn').hide();
-
-			callback(res);
+            }
 		});
 	}
 
-	/**
+
+		/**
 	 * 查询是否关注公众号接口
 	 * @param  	function 	callback 	查找后的回调
 	 * @return 	null
@@ -99,22 +90,93 @@ $(function() {
 			}
 		});
 	}
-
 	/**
-	 * 查找二维码情况
-	 * @param  function callback 查找后的回调
-	 * @return null
+	 * 获取手机短信验证码
+	 * @param 	object 		data 		数据
+	 * @param  	function 	callback 	查找后的回调
+	 * @return 	null
 	 */
-	Func.findActivityByEncode = function(callback) {
+	Func.getVerCode = function(data, callback) {
 
 		$('#loadingWrapper').show();
 
 		$.ajax({
-			url: api.findActivityByEncode,
-			type: 'GET',
+			url: api.getVerCode,
+			type: 'POST',
+			data: data,
 			headers: getHeader(),
 			dataType: 'json',
 			success: function(res) {
+				$('#loadingWrapper').hide();
+				callback(res);
+			}
+		});
+	}
+
+	/**
+	 * 获取手机短信验证码（彼特明）
+	 * @param 	object 		data 		数据
+	 * @param  	function 	callback 	查找后的回调
+	 * @return 	null
+	 */
+	Func.btmGetVerCode = function(data, callback) {
+
+		$('#loadingWrapper').show();
+
+		$.ajax({
+			url: api.btmGetVerCode,
+			type: 'POST',
+			data: data,
+			headers: getHeader(),
+			dataType: 'json',
+			success: function(res) {
+				$('#loadingWrapper').hide();
+				callback(res);
+			}
+		});
+	}
+
+	/**
+	 * 获取手机短信验证码（彼特明）
+	 * @param 	object 		data 		数据
+	 * @param  	function 	callback 	查找后的回调
+	 * @return 	null
+	 */
+	Func.checkPhonesUserStatus = function(data, callback) {
+
+		$('#loadingWrapper').show();
+
+		$.ajax({
+			url: api.checkPhonesUserStatus,
+			type: 'POST',
+			data: data,
+			headers: getHeader(),
+			dataType: 'json',
+			success: function(res) {
+				$('#loadingWrapper').hide();
+				callback(res);
+			}
+		});
+	}
+
+	/**
+	 * 消费者注册
+	 * @param 	object 		data 		数据
+	 * @param  	function 	callback 	查找后的回调
+	 * @return 	null
+	 */
+	Func.register = function(data, callback) {
+
+		$('#loadingWrapper').show();
+
+		$.ajax({
+			url: api.register,
+			type: 'POST',
+			data: data,
+			headers: getHeader(),
+			dataType: 'json',
+			success: function(res) {
+				$('#loadingWrapper').hide();
 				callback(res);
 			}
 		});
@@ -145,7 +207,6 @@ $(function() {
 	 * @return null
 	 */
 	Func.userCash = function(data, callback) {
-		$('#loadingWrapper').show();
 
 		$.ajax({
 			url: api.userCash,
@@ -221,7 +282,6 @@ $(function() {
 			$('#loadingWrapper').show();
 		}
 
-		console.log(api.lotteryRecord)
 		$.ajax({
 			url: api.lotteryRecord,
 			type: 'GET',
@@ -234,31 +294,6 @@ $(function() {
 		});
 	}
 
-
-	
-	/**
-	 * 获取企业中奖记录
-	 * @param  object 		data 		提交的数据
-	 * @param  function 	callback 	获取成功后的回调
-	 * @return null
-	 */
-	Func.companyLotteryRecord = function(data, callback) {
-
-		if (data.page === 1) {
-			$('#loadingWrapper').show();
-		}
-
-		$.ajax({
-			url: api.companyLotteryRecord,
-			type: 'GET',
-			data: data,
-			headers: getHeader(),
-			dataType: 'json',
-			success: function(res) {
-				callback(res);
-			}
-		});
-	}
 	/**
 	 * 获取优惠券列表
 	 * @param  object 		data 		提交的数据
@@ -353,70 +388,65 @@ $(function() {
 					// var random = Math.floor(Math.random() * res.data.length);
 					// var data = res.data[random];
 
-					if(res.data != "") {
-						Global.requestTempByAjax('/commonAct_1/temp/swiper.ejs', res.data, function (template) {
-							// $(ele).append('<img class="ad" src="' + data.picUrl + '" data-advId="' + data.advId + '" data-href="' + data.linkUrl + '" />');
+					Global.requestTempByAjax('/commonAct_1/temp/swiper.ejs', res.data, function(template) {
+						// $(ele).append('<img class="ad" src="' + data.picUrl + '" data-advId="' + data.advId + '" data-href="' + data.linkUrl + '" />');
 
-							$(ele).append(template);
+						$(ele).append(template);
 
-							var swiperSlide = $(ele).find('.swiper-slide');
+						var swiperSlide = $(ele).find('.swiper-slide');
 
-							if (swiperSlide.length <= 1) {
-								$(ele).find('.swiper-pagination').hide();
-							}
-							// debugger;
-							if (swiperSlide.length === 0) {
-								$(ele).prev().css({bottom: 0});
-								$(ele).hide();
-							}
+						if (swiperSlide.length <= 1) {
+							$(ele).find('.swiper-pagination').hide();
+						}
+						// debugger;
+						if (swiperSlide.length === 0) {
+							$(ele).prev().css({bottom: 0});
+							$(ele).hide();
+						}
 
-							var swiper = new Swiper('.swiper-container', {
-								pagination: {
-									el: '.swiper-pagination',
-								},
-								autoplay: true,
-								on: {
-									init: function () {
-										var index = this.activeIndex;
+						var swiper = new Swiper('.swiper-container', {
+							pagination: {
+								el: '.swiper-pagination',
+							},
+							autoplay: true,
+							on: {
+								init: function() {
+									var index = this.activeIndex;
 
-										var img = $(ele).find('.swiper-slide').eq(index).find('img');
+									var img = $(ele).find('.swiper-slide').eq(index).find('img');
 
-										if (img.attr('data-loading') === 'true') {
-											return;
-										}
-
-										img.attr('data-loading', 'true');
-
-										Func.browseRecord(img.attr('data-advId'), 0);
-
-										this.emit('transitionEnd'); //在初始化时触发一次transitionEnd事件
-									},
-									slideChangeTransitionEnd: function () {
-										var index = this.activeIndex;
-
-										var img = $(ele).find('.swiper-slide').eq(index).find('img');
-
-										if (img.attr('data-loading') === 'true') {
-											return;
-										}
-
-										img.attr('data-loading', 'true');
-
-										Func.browseRecord(img.attr('data-advId'), 0);
+									if (img.attr('data-loading') === 'true') {
+										return;
 									}
+
+									img.attr('data-loading', 'true');
+
+									Func.browseRecord(img.attr('data-advId'), 0);
+
+									this.emit('transitionEnd'); //在初始化时触发一次transitionEnd事件
 								},
-							});
+								slideChangeTransitionEnd: function() {
+									var index = this.activeIndex;
 
-							if (typeof callback !== 'undefined') {
-								callback(res.data);
-							}
+									var img = $(ele).find('.swiper-slide').eq(index).find('img');
 
-							// Func.browseRecord(data.advId, 0);
+									if (img.attr('data-loading') === 'true') {
+										return;
+									}
+
+									img.attr('data-loading', 'true');
+
+									Func.browseRecord(img.attr('data-advId'), 0);
+								}
+							},
 						});
-					} else {
-						$("#awardInforWrapper").css({bottom: 0});
-						$("#advertisement").css({background: "transparent"});
-					}
+
+						if (typeof callback !== 'undefined') {
+							callback(res.data);
+						}
+
+						// Func.browseRecord(data.advId, 0);
+					});
 
 				} else {
 					$(ele).prev().css({bottom: 0});
@@ -495,24 +525,6 @@ $(function() {
 	};
 
 	/**
-	 * 消费者自主核销
-	 * @return null
-	 */
-	Func.cancelCoupon = function(data, callback) {
-		
-		$.ajax({
-			url: api.cancelCoupon,
-			type: 'GET',
-			headers: getHeader(),
-			dataType: 'json',
-			data: data,
-			success: function(res) {
-				callback(res);
-			}
-		});
-	}
-
-	/**
 	 * 获取用户信息
 	 * @param  function 	callback 	提交成功后的回调
 	 * @return null
@@ -561,166 +573,67 @@ $(function() {
 				callback(res);
 			}
 		});
-	},
+	}
 
+    /**
+     * 查询手机号是否可以参与某活动
+	 * @param 	object 		phone		手机号
+     * @param  function 	callback 	提交成功后的回调
+     * @return null
+     */
+    Func.checkPhoneIsEnabelJoinAct = function(data,callback) {
+        $.ajax({
+            url: api.checkPhoneIsEnabelJoinAct,
+            type: 'GET',
+            headers: getHeader(),
+            dataType: 'json',
+			data:data,
+            success: function(res) {
+                callback(res);
+            }
+        });
+    }
+
+    /**
+     * 发起提现申请
+     * @param 	object 		amount		申请提现的金额
+     * @param  function 	callback 	提交成功后的回调
+     * @return null
+     */
+    Func.guestApplyWithdraw = function(data,callback) {
+        $.ajax({
+            url: api.guestApplyWithdraw,
+            type: 'GET',
+            headers: getHeader(),
+            dataType: 'json',
+            data:data,
+            success: function(res) {
+                callback(res);
+            }
+        });
+	}
+	
 	/**
-	 * 企业中奖纪录查询
-	 * 
+	 * 获取企业中奖记录
+	 * @param  object 		data 		提交的数据
+	 * @param  function 	callback 	获取成功后的回调
+	 * @return null
 	 */
-	  
 	Func.companyLotteryRecord = function(data, callback) {
+
+		if (data.page === 1) {
+			$('#loadingWrapper').show();
+		}
+
 		$.ajax({
 			url: api.companyLotteryRecord,
 			type: 'GET',
-			headers: getHeader(),
 			data: data,
-			dataType: 'json',
-			success: function(res) {
-				callback(res);
-			}
-		});
-	}
-
-	/**
-	 * 中奖记录滚动
-	 * @return null
-	 */
-	Func.awardScroll = function() {
-		
-
-		$('#awardInforScrollContent').append($('#awardInforScrollContent').html());
-
-		var awardInforScrollContentWidth = $('#awardInforScrollContent').width();
-		var left = 0;
-		
-		// debugger;
-		var timer = setInterval(function() {
-
-			left--;
-
-			if (Math.abs(left) >= awardInforScrollContentWidth - $(window).width()) {
-				left = 0;
-			}
-
-			$('#awardInforScrollContent').css({
-				left: left
-			});
-		}, 10);
-	}
-
-
-    /**
-     * 获取短信验证码
-     * @return null
-     */
-    Func.getVerCode = function(data,callback) {
-        $.ajax({
-            url: api.getVerCode,
-            type: 'GET',
-            headers: getHeader(),
-            data: data,
-            dataType: 'json',
-            success: function(res) {
-                callback(res);
-            }
-        });
-    }
-
-    /**
-     * (罗伯克啤酒)提交用户信息
-     * @return null
-     */
-    Func.addUserInfo = function(data,callback) {
-        $('#loadingWrapper').show();
-        $.ajax({
-            url: api.addUserInfo,
-            type: 'POST',
-            headers: getHeader(),
-            data: data,
-            dataType: 'json',
-            success: function(res) {
-                $('#loadingWrapper').hide();
-                callback(res);
-            }
-        });
-    }
-
-    /**
-     * (罗伯克啤酒)校验用户是否填写详细信息
-     * @return null
-     */
-    Func.checkUserInfo = function(callback) {
-        $('#loadingWrapper').show();
-        $.ajax({
-            url: api.checkUserInfo,
-            type: 'GET',
-            headers: getHeader(),
-            dataType: 'json',
-            success: function(res) {
-                callback(res);
-            }
-        });
-    }
-
-
-    /**
-     * 实物中大奖校验用户是否填写信息
-     * @return null
-     */
-    Func.checkUser = function(callback) {
-        $('#loadingWrapper').show();
-        $.ajax({
-            url: api.checkUser,
-            type: 'GET',
-            headers: getHeader(),
-            dataType: 'json',
-            success: function(res) {
-                callback(res);
-            }
-        });
-	}
-
-	/**
-     * 获取商城url
-     * @return null
-     */
-    Func.getShopUrl = function(getShopUrlData,callback) {
-        $('#loadingWrapper').show();
-        $.ajax({
-            url: api.getShopUrl,
-            type: 'GET',
-            headers: getHeader(),
-            dataType: 'json',
-			data:getShopUrlData,
-            success: function(res) {
-                callback(res);
-            }
-        });
-    }
-
-	/**
-	 * 抽奖
-	 * @param  function callback 抽奖后的回调函数
-	 * @return null
-	 */
-	Func.lottery = function(url, callback) {
-
-		if (isLottery) {
-			return;
-		}
-
-		isLottery = true;
-
-		$.ajax({
-			url: url,
-			type: 'GET',
 			headers: getHeader(),
 			dataType: 'json',
 			success: function(res) {
-				isLottery = false;
 				callback(res);
 			}
 		});
 	}	
-
 });
