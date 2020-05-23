056712d35080cae3ab4a2b55c30a6ad1753c3e70
huyao95 <huyao95@example.com>
2020-05-23 14:49:15 +0800

    5.23

--- public/jiujiuyiyuan/scripts/api.js
+++ public/jiujiuyiyuan/scripts/api.js
@@ -1,8 +1,8 @@
-var dev = 'http://192.168.1.57:8085/qdwz';
+var dev = 'http://192.168.1.41:8085/qdwz';
 var test = 'https://test-api.lxcyhd.com/qdwz';
 var production = 'https://api.lxcyhd.com/qdwz';
 
-var ip = production;
+var ip = dev;
 
 var api = {
 	validate :ip + '/consumerActivity/jjyy/validate',								//防伪次数接口
