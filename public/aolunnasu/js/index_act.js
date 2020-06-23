227da822d8b7daab14d20223aa0a7e8eb06b7b29
huyao95 <huyao95@example.com>
2020-06-23 10:43:34 +0800

    6.23奥伦纳素

--- public/aolunnasu/js/index_act.js
+++ public/aolunnasu/js/index_act.js
@@ -1,18 +1,12 @@
 $(function () {
 
-	// $('.submit').on('click',function() {
-	// 	$('.mask').fadeIn();
-	// 	$('.mask-con1').fadeIn();
-	// })
-
-
 	var str = window.location.href;
 	var search = "resultType=0";
 	var start = str.indexOf(search);//获得字符串的开始位置
 	var result = str.substring(start, start + search.length);//截取字符串
+
+	var status; //0未领奖 1已领奖 2领取失败
 	console.log(result)
-
-
 
 	if (result == "resultType=0") {
 		$('.text-title').html('验证为正品');
@@ -26,7 +20,6 @@ $(function () {
 			headers: getHeader(),
 			success: function (res) {
 				$('.scanNum').text(res.data.scanNum);
-
 				$('.scanDate').text(res.data.scanDate)
 			},
 			error: function (res) {
@@ -35,19 +28,21 @@ $(function () {
 
 		});
 
-
 		Func.createJoinActInfo(function (res) {
 			if (res.code == 200) {
 				findActivityByEncode()
+				status = 4
+			} if (res.code == 201) {
+				status = 5
+			} else if (res.code == 202) {
+				status = res.data.status
 			}
 		})
 
 	}
 
 
-
-
-
+	//参与抽奖
 	function findActivityByEncode() {
 		Func.findActivityByEncode(function (res) {
 			$('#loadingWrapper').hide();
@@ -57,9 +52,9 @@ $(function () {
 					$('#loadingWrapper').hide();
 					if (red.code === 200) {
 						$('.num').html(red.data.point.point);
-						$('.mask').fadeIn();
-						$('.mask-con2').fadeIn()
-
+						// $('.mask').fadeIn();
+						// $('.mask-con2').fadeIn()
+						status = 6
 						var prizeId = red.data.prizeId;
 						var score = red.data.point.point;
 						var lotteryId = red.data.lotteryId;
@@ -91,7 +86,17 @@ $(function () {
 
 	// 点击开启积分
 	$('.submit').on('click', function () {
-
+		console.log(status)
+		if (status == 6 || status == 0) {
+			$('.mask').fadeIn();
+			$('.mask-con2').fadeIn()
+		} else if (status == 5) {
+			$('.mask').fadeIn();
+			$('.mask-con5').fadeIn()
+		} else if (status == 1) {
+			$('.mask').fadeIn();
+			$('.mask-con5').fadeIn()
+		}
 
 	})
 
