1e344d4fd65f5dbf7d12b5cc03d7836afb371c01
huyao95 <huyao95@example.com>
2020-07-17 17:54:16 +0800

    7.17玖玖逸韻修改

--- public/jiujiuyiyuan/scripts/api.js
+++ public/jiujiuyiyuan/scripts/api.js
@@ -1,11 +1,11 @@
-var dev = 'http://192.168.1.41:8085/qdwz';
+var dev = 'http://192.168.1.57:8085/qdwz';
 var test = 'https://test-api.lxcyhd.com/qdwz';
 var production = 'https://api.lxcyhd.com/qdwz';
 
-var ip = dev;
+var ip = test;
 
 var api = {
-	validate :ip + '/consumerActivity/jjyy/validate',								//防伪次数接口
+	validate: ip + '/consumerActivity/jjyy/validate',								//防伪次数接口
 	findEncodeFunction: ip + '/consumerActivity/jjyy/encodeFunction', 			// 防伪溯源展示接口
-	
+
 };
